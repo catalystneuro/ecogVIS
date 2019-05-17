24efcceaa8390150cba4fb34091b2d9ffbe62a8a
Ben Dichter <ben.dichter@gmail.com>
2019-05-17 14:17:49 -0700

    fix setup

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
       name = 'ecogTSGUI',
       version = '1.0',
       description = 'python gui interface',
-      author = 'Ben Ditcher',
-      email = 'xyz@gmail.com',
-      scripts = ['ecog_ts_gui.py'],
+      author = 'Ben Dichter',
+      email = 'ben.dichter@gmail.com',
+      scripts = ['ecog_vis.py'],
       url = '',
       install_requires = ['PyQt5', 'matplotlib', 'cycler', 'scipy', 'numpy', 'h5py', 'pyqtgraph']
       
