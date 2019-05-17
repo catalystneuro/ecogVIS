b8cf79ccb68617e84d8048da69e1301b8fc525e0
Ben Dichter <ben.dichter@gmail.com>
2019-05-17 14:20:35 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
       email = 'ben.dichter@gmail.com',
       scripts = ['ecog_vis.py'],
       url = '',
-      install_requires = ['PyQt5', 'matplotlib', 'cycler', 'scipy', 'numpy', 'h5py', 'pyqtgraph']
+      install_requires = ['PyQt5', 'matplotlib', 'cycler', 'scipy', 'numpy', 'h5py', 'pyqtgraph', 'pandas']
       
       )
