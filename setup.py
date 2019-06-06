1ea6138fb30f30d0de8bede39908ae9f477e0ec0
root <luit_tauffer@hotmail.com>
2019-06-06 11:15:25 +0200

    update: setup.py still needs to install process_ecog properly

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 #setup.py file
 from setuptools import setup
+
+
 setup(
       name = 'ecogVIS',
-      version = '1.0',
+      version = '0.0.1',
       description = 'python gui interface',
       author = 'Luiz Tauffer and Ben Dichter',
       email = 'ben.dichter@gmail.com',
       scripts = ['ecog_vis.py'],
       url = '',
       install_requires = ['PyQt5', 'matplotlib', 'cycler', 'scipy', 'numpy', 'h5py', 'pyqtgraph', 'pandas', 'pynwb',
-                         'nwbext_ecog']
-      
+                          'nwbext_ecog', 'pyopengl'],
+      dependency_links=['http://github.com/luiztauffer/process_ecog/tarball/docker#egg=ecog'],
       )
+
+
+#git+https://github.com/luiztauffer/process_ecog.git@docker#egg=ecog
