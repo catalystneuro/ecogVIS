fa82c461fb98464cd94a1de2e481becae1fa8aa1
luiztauffer <luiz_tauffer@hotmail.com>
2019-07-24 12:38:54 +0200

    installation updates

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
+# Get the long description from the README file
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(name = 'ecogvis',
       version = '1.0.0',
-      description = 'ecog gui interface for python',
+      description = 'Timeseries visualizer for Electrocorticography (ECoG) signals stored in NWB files.',
+      long_description = long_description,
+      long_description_content_type = 'text/markdown',
       author = 'Luiz Tauffer and Ben Dichter',
       email = 'ben.dichter@gmail.com',
       packages=find_packages(),
       include_package_data = True,
       install_requires = ['PyQt5', 'matplotlib', 'cycler', 'scipy', 'numpy',
                           'h5py', 'pyqtgraph', 'pandas', 'pynwb',
-                          'nwbext_ecog', 'pyopengl', 'nomkl', 'scikit-learn',
-                          'numexpr'],
+                          'nwbext_ecog', 'pyopengl'],
       )
