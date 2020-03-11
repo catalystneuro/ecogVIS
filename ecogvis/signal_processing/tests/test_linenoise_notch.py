0c3985caec2cd9804c7867a3d4fd4a50f5adc1c3
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-03-11 12:15:18 -0400

    Update test_linenoise_notch.py

--- ecogvis/signal_processing/tests/test_linenoise_notch.py
+++ ecogvis/signal_processing/tests/test_linenoise_notch.py
@@ -1,6 +1,5 @@
 import numpy as np
-
-from ecog.signal_processing import linenoise_notch
+from ecogvis.signal_processing.linenoise_notch import linenoise_notch,apply_notches
 
 def test_linenoise_notch_return():
     """
@@ -10,3 +9,10 @@ def test_linenoise_notch_return():
     rate = 200
     Xh = linenoise_notch(X, rate)
     assert Xh.shape == X.shape
+
+def test_apply_notches():
+    X = np.random.randn(32, 1000)
+    rate = 200
+    notches = np.arange(60, 100, 60)
+    Xan = apply_notches(X, notches, rate)
+    assert Xan.shape == X.shape
