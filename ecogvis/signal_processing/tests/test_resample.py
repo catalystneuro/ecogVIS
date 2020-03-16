4e7bfd5cf4eafa172986eeac379c240e1da0ef1c
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-03-16 12:08:07 -0400

    Update test_resample.py

--- ecogvis/signal_processing/tests/test_resample.py
+++ ecogvis/signal_processing/tests/test_resample.py
@@ -12,5 +12,9 @@ def test_resample_same_sign_false_kind_zero():
     Xp = resample(X, 100, 200, kind=0)
     assert Xp.shape == (32, 1000)
 
+def test_resample_same_sign_false_kind_nonzero():
+    X = np.random.randn(32, 2000)
+    Xp = resample(X, 100, 200)
+    assert Xp.shape == (32, 1000)
 
 
