2dcdc86f2b3d86f4b4641af26fbb4774effc9f14
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-03-16 15:31:02 -0400

    Update test_detect_events.py

--- ecogvis/signal_processing/tests/test_detect_events.py
+++ ecogvis/signal_processing/tests/test_detect_events.py
@@ -1,6 +1,11 @@
+import numpy as np
+from pynwb import TimeSeries
 from ecogvis.signal_processing.detect_events import detect_events,threshcross
 
 def test_detect_events():
+    speaker_data = TimeSeries(name='speaker_data', data=np.random.rand(160,), unit='m', starting_time=0.0, rate=1.0)
+    mic_data = TimeSeries(name='mic_data', data=np.random.rand(160,), unit='m', starting_time=0.0, rate=1.0)
+    detect_events(speaker_data, mic_data)
     
     
 def test_threshcross():
