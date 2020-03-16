821ceb34e13025ce760a69f92e0fe731ec97c2ef
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-03-16 11:10:29 -0400

    Update test_resample.py

--- ecogvis/signal_processing/tests/test_resample.py
+++ ecogvis/signal_processing/tests/test_resample.py
@@ -1,21 +1,16 @@
 import numpy as np
+from ecogvis.signal_processing.resample import resample
 
-from ecog.signal_processing import resample
-
-def test_resample_shape():
+def test_resample_same_sign_true():
     X = np.random.randn(32, 2000)
-
-    Xp = resample(X, 100, 200)
+    Xp = resample(X, 100, 200, same_sign=True)
     assert Xp.shape == (32, 1000)
 
 
-def test_resample_ones():
-    chs = [2, 32, 100]
+def test_resample_same_sign_false_kind_zero():
+    X = np.random.randn(32, 2000)
+    Xp = resample(X, 100, 200, kind=0)
+    assert Xp.shape == (32, 1000)
 
-    ts = [50, 1001, 5077]
-    for ch in chs:
-        for t in ts:
-            X = np.ones((32, 2000))
 
-            Xp = resample(X, 100, 200)
-            assert np.allclose(Xp, 1.)
+
