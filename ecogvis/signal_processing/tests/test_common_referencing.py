af46d1e944419562e29b0f7419251cb836a6a238
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-03-16 13:39:36 -0400

    Update test_common_referencing.py

--- ecogvis/signal_processing/tests/test_common_referencing.py
+++ ecogvis/signal_processing/tests/test_common_referencing.py
@@ -3,9 +3,10 @@ from ecogvis.signal_processing.common_referencing import subtract_CAR,subtract_c
 def test_subtract_common_median_reference():
     X = np.random.randn(1000, 96, 500)
     Xp = subtract_common_median_reference(X, channel_axis=-2)
-    Xp.shape = X.shape
+    assert Xp.shape == X.shape
     
 def test_subtract_CAR():
     X = np.random.randn(167, 500)
-    subtract_CAR(X)
+    Xs = subtract_CAR(X)
+    assert Xs.shape == X.shape
     
