94cf44e30dbd7b1a1fe7918c5254d2e189c833b8
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-03-17 11:18:11 -0400

    Update test_periodogram.py

--- ecogvis/signal_processing/tests/test_periodogram.py
+++ ecogvis/signal_processing/tests/test_periodogram.py
@@ -6,119 +6,89 @@ from pynwb import NWBHDF5IO
 from pynwb.ecephys import ElectricalSeries
 from pynwb.ecephys import LFP
 from ecogvis.signal_processing.periodogram import psd_estimate
+import unittest
 
 
-def test_psd_estimate_raw():
-    
-    start_time = datetime(2017, 4, 3, 11, tzinfo=tzlocal())
-    create_date = datetime(2017, 4, 15, 12, tzinfo=tzlocal())
+class periodogramTestCase(unittest.TestCase):
 
-    nwbfile = NWBFile('my first synthetic recording', 'EXAMPLE_ID', datetime.now(tzlocal()),
-                      experimenter='Dr. Bilbo Baggins',
-                      lab='Bag End Laboratory',
-                      institution='University of Middle Earth at the Shire',
-                      experiment_description='I went on an adventure with thirteen dwarves to reclaim vast treasures.',
-                      session_id='LONELYMTN')
+    def setUp(self):
+        
+        start_time = datetime(2017, 4, 3, 11, tzinfo=tzlocal())
+        create_date = datetime(2017, 4, 15, 12, tzinfo=tzlocal())
 
-    device = nwbfile.create_device(name='trodes_rig123')
+        self.nwbfile = NWBFile('my first synthetic recording', 'EXAMPLE_ID', datetime.now(tzlocal()),
+                          experimenter='Dr. Bilbo Baggins',
+                          lab='Bag End Laboratory',
+                          institution='University of Middle Earth at the Shire',
+                          experiment_description='I went on an adventure with thirteen dwarves to reclaim vast treasures.',
+                          session_id='LONELYMTN')
 
-    electrode_name = 'tetrode1'
-    description = "an example tetrode"
-    location = "somewhere in the hippocampus"
+        device = self.nwbfile.create_device(name='trodes_rig123')
 
-    electrode_group = nwbfile.create_electrode_group(electrode_name,
-                                                     description=description,
-                                                     location=location,
-                                                     device=device)
+        electrode_name = 'tetrode1'
+        description = "an example tetrode"
+        location = "somewhere in the hippocampus"
 
-    for idx in [1, 2, 3, 4]:
-        nwbfile.add_electrode(id=idx,
-                              x=1.0, y=2.0, z=3.0,
-                              imp=float(-idx),
-                              location='CA1', filtering='none',
-                              group=electrode_group)
+        electrode_group = self.nwbfile.create_electrode_group(electrode_name,
+                                                         description=description,
+                                                         location=location,
+                                                         device=device)
 
-    electrode_table_region = nwbfile.create_electrode_table_region([0, 2], 'the first and third electrodes')
+        for idx in [1, 2, 3, 4]:
+            self.nwbfile.add_electrode(id=idx,
+                                  x=1.0, y=2.0, z=3.0,
+                                  imp=float(-idx),
+                                  location='CA1', filtering='none',
+                                  group=electrode_group)
 
-    rate = 10.0
-    np.random.seed(1234)
-    data_len = 1000
-    ephys_data = np.random.rand(data_len * 2).reshape((data_len, 2))
-    ephys_timestamps = np.arange(data_len) / rate
+        self.electrode_table_region = self.nwbfile.create_electrode_table_region([0, 2], 'the first and third electrodes')
 
-    ephys_ts = ElectricalSeries('ElectricalSeries',
-                                ephys_data,
-                                electrode_table_region,
-                                starting_time=ephys_timestamps[0],
-                                rate=rate,
-                                resolution=0.001,
-                                comments="This data was randomly generated with numpy, using 1234 as the seed",
-                                description="Random numbers generated with numpy.random.rand")
-    
-    nwbfile.add_acquisition(ephys_ts)
+        self.rate = 10.0
+        np.random.seed(1234)
+        data_len = 1000
+        self.ephys_data = np.random.rand(data_len * 2).reshape((data_len, 2))
+        self.ephys_timestamps = np.arange(data_len) / rate
 
-    with NWBHDF5IO('ecephys_example.nwb', 'w') as io:
-        io.write(nwbfile)
+        
 
-    psd_estimate('ecephys_example.nwb', 'raw')
-    
+    def test_psd_estimate_raw(self):
+
+        ephys_ts = ElectricalSeries('ElectricalSeries',
+                                    self.ephys_data,
+                                    self.electrode_table_region,
+                                    starting_time=self.ephys_timestamps[0],
+                                    rate=self.rate,
+                                    resolution=0.001,
+                                    comments="This data was randomly generated with numpy, using 1234 as the seed",
+                                    description="Random numbers generated with numpy.random.rand")
+
+        self.nwbfile.add_acquisition(ephys_ts)
+
+        with NWBHDF5IO('ecephys_example.nwb', 'w') as io:
+            io.write(self.nwbfile)
+
+        psd_estimate('ecephys_example.nwb', 'raw')
 
 
-def test_psd_estimate_preprocessed():
-    
-    start_time = datetime(2017, 4, 3, 11, tzinfo=tzlocal())
-    create_date = datetime(2017, 4, 15, 12, tzinfo=tzlocal())
+    def test_psd_estimate_preprocessed(self):
 
-    nwbfile = NWBFile('my first synthetic recording', 'EXAMPLE_ID', datetime.now(tzlocal()),
-                      experimenter='Dr. Bilbo Baggins',
-                      lab='Bag End Laboratory',
-                      institution='University of Middle Earth at the Shire',
-                      experiment_description='I went on an adventure with thirteen dwarves to reclaim vast treasures.',
-                      session_id='LONELYMTN')
-    
-    device = nwbfile.create_device(name='trodes_rig123')
+        ephys_ts = ElectricalSeries('preprocessed',
+                                    self.ephys_data,
+                                    self.electrode_table_region,
+                                    starting_time=self.ephys_timestamps[0],
+                                    rate=self.rate,
+                                    resolution=0.001,
+                                    comments="This data was randomly generated with numpy, using 1234 as the seed",
+                                    description="Random numbers generated with numpy.random.rand")
 
-    electrode_name = 'tetrode1'
-    description = "an example tetrode"
-    location = "somewhere in the hippocampus"
+        lfp = LFP(ephys_ts)
 
-    electrode_group = nwbfile.create_electrode_group(electrode_name,
-                                                     description=description,
-                                                     location=location,
-                                                     device=device)
+        ecephys_module = self.nwbfile.create_processing_module(name='ecephys',
+                                                       description='preprocessed ecephys data')
 
-    for idx in [1, 2, 3, 4]:
-        nwbfile.add_electrode(id=idx,
-                              x=1.0, y=2.0, z=3.0,
-                              imp=float(-idx),
-                              location='CA1', filtering='none',
-                              group=electrode_group)
+        self.nwbfile.processing['ecephys'].add(lfp)
 
-    electrode_table_region = nwbfile.create_electrode_table_region([0, 2], 'the first and third electrodes')
+        with NWBHDF5IO('ecephys_example.nwb', 'w') as io:
+            io.write(self.nwbfile)
 
-    rate = 10.0
-    np.random.seed(1234)
-    data_len = 1000
-    ephys_data = np.random.rand(data_len * 2).reshape((data_len, 2))
-    ephys_timestamps = np.arange(data_len) / rate
-
-    ephys_ts = ElectricalSeries('preprocessed',
-                                ephys_data,
-                                electrode_table_region,
-                                starting_time=ephys_timestamps[0],
-                                rate=rate,
-                                resolution=0.001,
-                                comments="This data was randomly generated with numpy, using 1234 as the seed",
-                                description="Random numbers generated with numpy.random.rand")
-    
-    lfp = LFP(ephys_ts)
-    
-    ecephys_module = nwbfile.create_processing_module(name='ecephys',
-                                                   description='preprocessed ecephys data')
-    
-    nwbfile.processing['ecephys'].add(lfp)
-
-    with NWBHDF5IO('ecephys_example.nwb', 'w') as io:
-        io.write(nwbfile)
-    
-    psd_estimate('ecephys_example.nwb', 'preprocessed')
+        psd_estimate('ecephys_example.nwb', 'preprocessed')
