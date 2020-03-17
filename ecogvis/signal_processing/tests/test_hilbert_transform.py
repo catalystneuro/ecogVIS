c6364797005f22c1ea5e2bb56f20bda717f3ed06
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-03-17 10:26:53 -0400

    Update test_hilbert_transform.py

--- ecogvis/signal_processing/tests/test_hilbert_transform.py
+++ ecogvis/signal_processing/tests/test_hilbert_transform.py
@@ -1,40 +1,45 @@
 import numpy as np
 from ecogvis.signal_processing.hilbert_transform import (hilbert_transform,
         gaussian, hamming)
+import unittest
 
-def test_hilbert_return():
-    """
-    Test the return shape and dtype.
-    """
-    X = np.random.randn(32, 1000)
-    rate = 200
-    filters = [gaussian(X, rate, 100, 5),
-               hamming(X, rate, 60, 70)]
-    Xh = hilbert_transform(X, rate, filters)
-    assert Xh.shape == (len(filters), X.shape[0], X.shape[1])
-    assert Xh.dtype == np.complex
+class HilbertTestCase(unittest.TestCase):
 
-    Xh = hilbert_transform(X, rate)
-    assert Xh.shape == X.shape
-    assert Xh.dtype == np.complex
+    def setUp(self):
+        self.X = np.random.randn(32, 1000)
+        self.rate = 200
 
+        
+    def test_hilbert_return(self):
+        """
+        Test the return shape and dtype.
+        """
+        
+        filters = [gaussian(self.X, self.rate, 100, 5),
+                   hamming(self.X, self.rate, 60, 70)]
+        Xh = hilbert_transform(self.X, self.rate, filters)
+        assert Xh.shape == (len(filters), self.X.shape[0], self.X.shape[1])
+        assert Xh.dtype == np.complex
 
-def test_gaussian():
-    X = np.random.randn(32, 1000)
-    rate = 200
-    center = 100
-    sd = 5
-    Xg = gaussian(X, rate, center, sd)
-    assert Xg.shape[0] == X.shape[1]
-    assert Xg.dtype == 'float64'
+        Xh = hilbert_transform(self.X, self.rate)
+        assert Xh.shape == self.X.shape
+        assert Xh.dtype == np.complex
 
 
-def test_hamming():
-    X = np.random.randn(32, 1000)
-    rate = 200
-    min_freq = 60
-    max_freq = 70
-    Xham = hamming(X, rate, min_freq, max_freq)
-    assert Xham.shape[0] == X.shape[1]
-    assert Xham.dtype == 'float64'
-    
+    def test_gaussian(self):
+       
+        center = 100
+        sd = 5
+        Xg = gaussian(self.X, self.rate, center, sd)
+        assert Xg.shape[0] == self.X.shape[1]
+        assert Xg.dtype == 'float64'
+
+
+    def test_hamming(self):
+        
+        min_freq = 60
+        max_freq = 70
+        Xham = hamming(self.X, self.rate, min_freq, max_freq)
+        assert Xham.shape[0] == self.X.shape[1]
+        assert Xham.dtype == 'float64'
+
