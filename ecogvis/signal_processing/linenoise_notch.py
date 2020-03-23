7eda300d1c771e1bf9ce872d7a3c2588472d4e81
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-03-23 15:07:16 -0400

    Delete linenoise_notch.py

--- ecogvis/signal_processing/linenoise_notch.py
+++
@@ -1,9 +0,0 @@
-from process_nwb.linenoise_notch import apply_notches
-from process_nwb.linenoise_notch import apply_linenoise_notch as linenoise_notch
-from __future__ import division
-import numpy as np
-from scipy.signal import firwin2, filtfilt
-from .fft import rfftfreq, rfft, irfft
-
-__all__ = ['linenoise_notch']
-__authors__ = "Alex Bujan"
