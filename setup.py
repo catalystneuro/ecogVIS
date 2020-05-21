e277e3a8968ec025e73c581db7214f62067e416a
luiztauffer <luiz_tauffer@hotmail.com>
2020-05-21 15:57:22 +0700

    fix tqdm

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     include_package_data=True,
     install_requires=['PyQt5', 'matplotlib', 'cycler', 'scipy', 'numpy',
                       'h5py', 'pyqtgraph', 'pandas', 'pynwb>=1.1.2', 'PyYAML',
-                      'nwbext_ecog==0.7.2', 'ndx-spectrum',
+                      'nwbext_ecog==0.7.2', 'ndx-spectrum', 'tqdm'
                       'pyopengl', 'process_nwb', 'tdt'],
     entry_points={
         'console_scripts': ['ecogvis=ecogvis.ecogvis:cmd_line_shortcut'],
