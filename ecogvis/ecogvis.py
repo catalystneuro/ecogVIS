9b441bf2480b695c7a25d761685f87f5dbeb0125
luiztauffer <luiz_tauffer@hotmail.com>
2020-07-01 22:39:44 +0700

    audio event detector working even if no microphone data is present

--- ecogvis/ecogvis.py
+++ ecogvis/ecogvis.py
@@ -622,8 +622,7 @@ class Application(QMainWindow):
         # Test if trials already exist
         if 'TimeIntervals_speaker' not in self.model.nwb.intervals:
             # Test if file contains audio signals
-            # if ('microphone' in self.model.nwb.acquisition) and ('speaker1' in self.model.nwb.stimulus):
-            if any(name in self.model.nwb.acquisition for name in ['anin4', 'microphone']) and ('speaker1' in self.model.nwb.stimulus):
+            if any(name in self.model.nwb.stimulus for name in ['speaker1', 'speaker2']):
                 w = AudioEventDetection(parent=self)
                 if w.value == 1:  # Trial times were detected
                     self.model.refresh_file()  # re-opens the file, now with new data
