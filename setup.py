7f6c493e5575cd7e99db1368f34e005dc6863d5e
luiztauffer <luiz_tauffer@hotmail.com>
2020-07-16 17:33:28 +0700

    patch version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='ecogvis',
-    version='1.2.1',
+    version='1.2.2',
     description='Timeseries visualizer for Electrocorticography (ECoG) signals stored in NWB files.',
     long_description=long_description,
     long_description_content_type='text/markdown',
